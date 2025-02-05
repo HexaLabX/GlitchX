a4ce2c672901f37613b326f7f4d1821a2356aff1
Hexaz4 <hexalab.official@gmail.com>
2025-02-04 23:12:19 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,4 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  base: "/"
 })
